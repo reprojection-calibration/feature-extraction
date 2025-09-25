dc2f62fdbd6e544b12b9b479ec64dc43f8d88c51
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-25 08:46:06 -0400

    temporarily add aprid gird gen python code

---
+++ april_tag_grid_gen.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+tag = np.full((5, 5), "d", dtype='U1')
+
+tag = np.pad(tag, (1), 'constant', constant_values=("w"))
+tag = np.pad(tag, (1), 'constant', constant_values=("b"))
+
+black_white_tag = np.pad(tag, (1), 'constant', constant_values=("w"))
+black_white_tag = np.pad(black_white_tag, (1), 'constant', constant_values=("w"))
+
+black_white_tag[0 + 1][0 + 1] = "b"
+black_white_tag[0 + 1][0] = "b"
+black_white_tag[0][0 + 1] = "b"
+
+black_white_tag[0 + 1][-1 - 1] = "b"
+black_white_tag[0][-1 - 1] = "b"
+black_white_tag[0 + 1][-1] = "b"
+
+black_white_tag[-1 - 1][0 + 1] = "b"
+black_white_tag[-1][0 + 1] = "b"
+black_white_tag[-1 - 1][0] = "b"
+
+black_white_tag[-1 - 1][-1 - 1] = "b"
+black_white_tag[-1][-1 - 1] = "b"
+black_white_tag[-1 - 1][-1] = "b"
+
+print(black_white_tag)
+
+black_white_code = ["".join(item) for item in black_white_tag.astype(str)]
+black_white_code = "".join(black_white_code)
+print(black_white_code)
