0d8c439590c5ecb4a17d29b747c07afcd70a7bbb
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-26 01:47:37 -0400

    use orginal generated c files

---
+++ code/src/apriltag/tagCustom36h11.h
@@ -0,0 +1,17 @@
+#ifndef _TAGCustom36H11
+#define _TAGCustom36H11
+
+#include "apriltag.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apriltag_family_t *tagCustom36h11_create();
+void tagCustom36h11_destroy(apriltag_family_t *tf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- code/src/target_extractors_april_tag.test.cpp
+++ code/src/target_extractors_april_tag.test.cpp
@@ -1,6 +1,11 @@
 
 #include <gtest/gtest.h>
 
+extern "C" {
+#include "apriltag/apriltag.h"
+#include "apriltag/tagCustom36h11.h"
+}
+
 namespace reprojection_calibration::feature_extraction {}  // namespace reprojection_calibration::feature_extraction
 
 using namespace reprojection_calibration::feature_extraction;
