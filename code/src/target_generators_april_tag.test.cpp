3b0a35b214c1b525e8925532b8c5e4135a236453
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-26 01:14:27 -0400

    adjust tests to 3611

--- code/src/target_generators_april_tag.test.cpp
+++ code/src/target_generators_april_tag.test.cpp
@@ -2,7 +2,6 @@
 
 #include <gtest/gtest.h>
 
-#include "april_tag_family_custom_25h9.hpp"
 #include "april_tag_family_custom_36h11.hpp"
 
 using namespace reprojection_calibration::feature_extraction;
@@ -12,24 +11,24 @@ TEST(TargetGeneratorsAprilTag, TestGenerateAprilBoard) {
     int const bit_size_pixel{10};
     cv::Mat const april_board{GenerateAprilBoard(pattern_size, bit_size_pixel, april_tag::custom36h11)};
 
-    EXPECT_EQ(april_board.rows, 390);
-    EXPECT_EQ(april_board.cols, 520);
+    EXPECT_EQ(april_board.rows, 420);
+    EXPECT_EQ(april_board.cols, 560);
 }
 
 TEST(TargetGeneratorsAprilTag, TestGenerateAprilTag) {
-    Eigen::MatrixXi const code_matrix{CalculateCodeMatrix(25, april_tag::custom25h9[0])};
+    Eigen::MatrixXi const code_matrix{CalculateCodeMatrix(36, april_tag::custom36h11[0])};
     int const bit_size_pixel{10};
     cv::Mat const april_tag{GenerateAprilTag(code_matrix, bit_size_pixel)};
 
     // TODO(Jack): Would it be better to calculate these values? Make sure we are consistent across all board types!
-    EXPECT_EQ(april_tag.rows, 130);
-    EXPECT_EQ(april_tag.cols, 130);
+    EXPECT_EQ(april_tag.rows, 140);
+    EXPECT_EQ(april_tag.cols, 140);
 }
 
 TEST(TargetGeneratorsAprilTag, TestCalculateCodeMatrix) {
-    Eigen::MatrixXi const code_matrix{CalculateCodeMatrix(25, april_tag::custom25h9[0])};
+    Eigen::MatrixXi const code_matrix{CalculateCodeMatrix(36, april_tag::custom36h11[0])};
 
     // Check two properties of the matrix and hope if anything in the implementation breaks these catch it -_-
-    EXPECT_EQ(code_matrix.sum(), 11);                                                            // Heuristic
-    EXPECT_TRUE(code_matrix.row(4).isApprox(Eigen::Vector<int, 5>{0, 0, 0, 1, 1}.transpose()));  // Heuristic
+    EXPECT_EQ(code_matrix.sum(), 17);                                                               // Heuristic
+    EXPECT_TRUE(code_matrix.row(5).isApprox(Eigen::Vector<int, 6>{1, 1, 1, 0, 0, 1}.transpose()));  // Heuristic
 }
