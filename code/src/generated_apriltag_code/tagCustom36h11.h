efcf5f6a26e6042dc13a65c3b50c31760c3e9d30
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-26 09:25:34 -0400

    clang format c code

--- code/src/generated_apriltag_code/tagCustom36h11.h
+++ code/src/generated_apriltag_code/tagCustom36h11.h
@@ -3,15 +3,15 @@
 
 // WARN(Jack): The relative path "apriltag/" needs to be added here because we are using the external installed package!
 // If you look at the generated code from the source repo (https://github.com/AprilRobotics/apriltag-generation) the
-// include will just be #include "apriltag.h"
+// include will just be #include "apriltag.h". Code was also clang formatted!
 #include <apriltag/apriltag.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-apriltag_family_t *tagCustom36h11_create();
-void tagCustom36h11_destroy(apriltag_family_t *tf);
+apriltag_family_t* tagCustom36h11_create();
+void tagCustom36h11_destroy(apriltag_family_t* tf);
 
 #ifdef __cplusplus
 }
