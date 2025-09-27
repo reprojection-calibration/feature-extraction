0e51ae343c36a9c061630a5dd7c16216e72ada26
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-27 01:08:27 -0400

    working subpix refinement

--- code/src/target_extractors_april_tag.test.cpp
+++ code/src/target_extractors_april_tag.test.cpp
@@ -44,13 +44,41 @@ TEST(TargetExtractorsAprilTag, HHH) {
         CalculateCodeMatrix(tag_family_handler.tag_family->nbits, tag_family_handler.tag_family->codes[0])};
     int const bit_size_pixel{10};
     cv::Mat const april_tag{GenerateAprilTag(bit_size_pixel, code_matrix)};
+    cv::Mat const april_tag_for_subpix{april_tag.clone()};
 
     AprilTagDetections const raw_detections{tag_detector.Detect(april_tag)};
     AprilTagDetection const detection{raw_detections[0]};
 
+    // Draw extracted points
     for (Eigen::Index i{0}; i < 4; ++i) {
         Eigen::Vector2d const p{detection.p.row(i)};
         cv::circle(april_tag, cv::Point(p(0), p(1)), 1, cv::Scalar(127), 1, cv::LINE_8);
+    }
+
+    // Draw homography transformed points
+    Eigen::Matrix<double, 4, 2> corners{
+        {-1, 1}, {1, 1}, {1, -1}, {-1, -1}};  // Technically ints but to do the math we need to be doubles
+    corners *= (4.5 / 3.5);
+    Eigen::Matrix<double, 4, 2> xxx{
+        (detection.H * corners.rowwise().homogeneous().transpose()).colwise().hnormalized().transpose()};
+
+    for (Eigen::Index i{0}; i < 4; ++i) {
+        Eigen::Vector2d const p{xxx.row(i)};
+        cv::circle(april_tag, cv::Point(p(0), p(1)), 1, cv::Scalar(127), 1, cv::LINE_8);
+    }
+
+    // Do subpixel refinement
+    std::vector<cv::Point2f> cv_corners;
+    for (Eigen::Index i{0}; i < 4; ++i) {
+        Eigen::Vector2d const p{xxx.row(i)};
+        cv_corners.push_back(cv::Point2f{static_cast<float>(p(0)), static_cast<float>(p(1))});
+    }
+
+    cv::cornerSubPix(april_tag_for_subpix, cv_corners, cv::Size(11, 11), cv::Size(-1, -1),
+                     cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.1));
+
+    for (int i{0}; i < 4; ++i) {
+        cv::circle(april_tag_for_subpix, cv_corners[i], 1, cv::Scalar(127), 1, cv::LINE_8);
     }
 
     EXPECT_EQ(1, 2);
