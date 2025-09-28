65619a0abd81cb015961ae1c02b391ece6dc0cce
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-28 09:55:59 -0400

    solid corner

--- april_tag_grid_gen.py
+++ april_tag_grid_gen.py
@@ -1,31 +1,20 @@
 import numpy as np
 
-tag = np.full((6, 6), "d", dtype='U1')
+tag = np.full((5, 5), "d", dtype='U1')
 
 tag = np.pad(tag, (1), 'constant', constant_values=("w"))
 tag = np.pad(tag, (1), 'constant', constant_values=("b"))
 
-black_white_tag = np.pad(tag, (1), 'constant', constant_values=("w"))
-black_white_tag = np.pad(black_white_tag, (1), 'constant', constant_values=("w"))
+tag = np.pad(tag, (1), 'constant', constant_values=("w"))
+tag = np.pad(tag, (1), 'constant', constant_values=("w"))
 
-black_white_tag[0 + 1][0 + 1] = "b"
-black_white_tag[0 + 1][0] = "b"
-black_white_tag[0][0 + 1] = "b"
+tag[0:2,0:2] = "b"
+tag[0:2,-2:] = "b"
+tag[-2:,0:2] = "b"
+tag[-2:,-2:] = "b"
 
-black_white_tag[0 + 1][-1 - 1] = "b"
-black_white_tag[0][-1 - 1] = "b"
-black_white_tag[0 + 1][-1] = "b"
+print(tag)
 
-black_white_tag[-1 - 1][0 + 1] = "b"
-black_white_tag[-1][0 + 1] = "b"
-black_white_tag[-1 - 1][0] = "b"
-
-black_white_tag[-1 - 1][-1 - 1] = "b"
-black_white_tag[-1][-1 - 1] = "b"
-black_white_tag[-1 - 1][-1] = "b"
-
-print(black_white_tag)
-
-black_white_code = ["".join(item) for item in black_white_tag.astype(str)]
-black_white_code = "".join(black_white_code)
-print(black_white_code)
+tag_code = ["".join(item) for item in tag.astype(str)]
+tag_code = "".join(tag_code)
+print(tag_code)
