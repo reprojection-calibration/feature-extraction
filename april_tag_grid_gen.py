19968a2d96c79f20840ac9e5ba57ade4f8a67978
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-29 01:52:57 -0400

    solid black corner

--- april_tag_grid_gen.py
+++ april_tag_grid_gen.py
@@ -4,26 +4,14 @@ tag = np.full((6, 6), "d", dtype='U1')
 
 tag = np.pad(tag, (1), 'constant', constant_values=("w"))
 tag = np.pad(tag, (1), 'constant', constant_values=("b"))
-tag = np.pad(tag, (1), 'constant', constant_values=("w"))
-
-tag[0, 0:2] = "b"
-tag[0:2, 0] = "b"
-
-tag[0, -2:] = "b"
-tag[0:2, -1] = "b"
-
-tag[-1, 0:2] = "b"
-tag[-2:, 0] = "b"
-
-tag[-1, -2:] = "b"
-tag[-2:, -1] = "b"
 
 tag = np.pad(tag, (1), 'constant', constant_values=("w"))
+tag = np.pad(tag, (1), 'constant', constant_values=("w"))
 
-tag[0:1, 0:1] = "b"
-tag[0:1, -1:] = "b"
-tag[-1:, 0:1] = "b"
-tag[-1:, -1:] = "b"
+tag[0:2, 0:2] = "b"
+tag[0:2, -2:] = "b"
+tag[-2:, 0:2] = "b"
+tag[-2:, -2:] = "b"
 
 print(tag)
 
