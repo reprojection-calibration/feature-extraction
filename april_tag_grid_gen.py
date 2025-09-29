4f1680f0fe849e90388a1a3c06605c88410465fa
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-29 01:06:34 -0400

    corner square

--- april_tag_grid_gen.py
+++ april_tag_grid_gen.py
@@ -1,17 +1,29 @@
 import numpy as np
 
-tag = np.full((5, 5), "d", dtype='U1')
+tag = np.full((6, 6), "d", dtype='U1')
 
 tag = np.pad(tag, (1), 'constant', constant_values=("w"))
 tag = np.pad(tag, (1), 'constant', constant_values=("b"))
-
-tag = np.pad(tag, (1), 'constant', constant_values=("w"))
 tag = np.pad(tag, (1), 'constant', constant_values=("w"))
 
-tag[0:2,0:2] = "b"
-tag[0:2,-2:] = "b"
-tag[-2:,0:2] = "b"
-tag[-2:,-2:] = "b"
+tag[0, 0:2] = "b"
+tag[0:2, 0] = "b"
+
+tag[0, -2:] = "b"
+tag[0:2, -1] = "b"
+
+tag[-1, 0:2] = "b"
+tag[-2:, 0] = "b"
+
+tag[-1, -2:] = "b"
+tag[-2:, -1] = "b"
+
+tag = np.pad(tag, (1), 'constant', constant_values=("w"))
+
+tag[0:1, 0:1] = "b"
+tag[0:1, -1:] = "b"
+tag[-1:, 0:1] = "b"
+tag[-1:, -1:] = "b"
 
 print(tag)
 
