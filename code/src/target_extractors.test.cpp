8d7b7efc9d95d2485f792fd5d78ee0b1ab17c55a
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-30 03:03:13 -0400

    refactor april grid extractor into others

--- code/src/target_extractors.test.cpp
+++ code/src/target_extractors.test.cpp
@@ -3,10 +3,15 @@
 #include <gtest/gtest.h>
 
 #include "target_generators.hpp"
+#include "target_generators_april_tag.hpp"
+
+extern "C" {
+#include "feature_extraction/generated_apriltag_code/tagCustom36h11.h"
+}
 
 using namespace reprojection_calibration::feature_extraction;
 
-TEST(TargetExtractors, TestExtractCheckerboardFeatures) {
+TEST(TargetExtractors, TestCheckerboardExtractor) {
     cv::Size const pattern_size{4, 3};  // (width, height) == (cols, rows)
     int const square_size_pixels{50};
     cv::Mat const image{GenerateCheckerboard(pattern_size, square_size_pixels)};
@@ -21,7 +26,7 @@ TEST(TargetExtractors, TestExtractCheckerboardFeatures) {
     EXPECT_TRUE(pixels->row(11).isApprox(Eigen::Vector2d{250, 200}.transpose(), 1e-6));  // Last pixel - heuristic
 }
 
-TEST(TargetExtractors, TestExtractCirclegridFeatures) {
+TEST(TargetExtractors, TestCircleGridExtractor) {
     cv::Size const pattern_size{4, 3};
     int const circle_radius_pixels{25};
     int const circle_spacing_pixels{20};  // Between circle edges
@@ -37,7 +42,7 @@ TEST(TargetExtractors, TestExtractCirclegridFeatures) {
     EXPECT_TRUE(pixels->row(11).isApprox(Eigen::Vector2d{55, 55}.transpose(), 1e-6));   // Last pixel - heuristic
 }
 
-TEST(TargetExtractors, TestExtractCirclegridFeaturesAsymmetric) {
+TEST(TargetExtractors, TestCircleGridExtractorAsymmetric) {
     // Refactor to use cv::Size
     // WARN(Jack): Must be even (rows)! See comment below.
     // WARN(Jack): Must be an odd number (cols) to prevent 180 degree rotation symmetry!
@@ -60,4 +65,26 @@ TEST(TargetExtractors, TestExtractCirclegridFeaturesAsymmetric) {
               (pattern_size.width * pattern_size.height) / 2);  // NOTE(Jack): Divide by two due to asymmetry!
     EXPECT_TRUE(pixels->row(0).isApprox(Eigen::Vector2d{475, 55}.transpose(), 1e-6));   // First pixel - heuristic
     EXPECT_TRUE(pixels->row(20).isApprox(Eigen::Vector2d{55, 335}.transpose(), 1e-6));  // Last pixel - heuristic
+}
+
+TEST(TargetExtractorsAprilTag, TestAprilGrid3Extractor) {
+    AprilTagFamily const tag_family_handler{tagCustom36h11_create(), tagCustom36h11_destroy};
+    Eigen::MatrixXi const code_matrix{
+        CalculateCodeMatrix(tag_family_handler.tag_family->nbits, tag_family_handler.tag_family->codes[0])};
+    int const bit_size_pixel{10};
+    cv::Mat const april_tag{GenerateAprilTag(bit_size_pixel, code_matrix)};
+
+    cv::Size const pattern_size{7, 6};  // WARN(Jack): Not actually needed here yet
+    auto const extractor{AprilGrid3Extractor{pattern_size}};
+
+    std::optional<Eigen::MatrixX2d> const pixels{extractor.Extract(april_tag)};
+
+    EXPECT_TRUE(pixels.has_value());
+    EXPECT_EQ(pixels->rows(), 4);  // One tag
+
+    Eigen::Matrix<double, 4, 2> const gt_pixels{{19.819417953491211, 119.27910614013672},
+                                                {119.13014984130859, 119.13014984130859},
+                                                {119.27910614013672, 19.819416046142578},
+                                                {19.685731887817383, 19.685731887817383}};
+    EXPECT_TRUE(pixels.value().isApprox(gt_pixels, 1e-6));
 }
