9dc958ffa9ddfa6f34564a6632c56d621d64bb9f
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-03 15:01:20 +0200

    order variables

--- building/local/run_webcam_demo.sh
+++ building/local/run_webcam_demo.sh
@@ -7,4 +7,12 @@ TAG=feature-extraction:release
 
 echo "Running container from image '$TAG'..."
 xhost +
-docker run --name webcam_demo --device=/dev/video0:/dev/video0 -e DISPLAY=:0.0 -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev:/dev --privileged --rm --volume ${SCRIPT_FOLDER}/../../:/temporary ${TAG}
+docker run \
+  --env DISPLAY=:0.0 \
+  --name webcam_demo \
+  --privileged \
+  --rm \
+  --volume /dev:/dev \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix \
+  --volume ${SCRIPT_FOLDER}/../../:/temporary \
+  ${TAG}
